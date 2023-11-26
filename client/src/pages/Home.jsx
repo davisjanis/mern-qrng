26818e46d4c3dbb56b8baaef0d1fee77cc6da170
davisjanis <davislarins@gmail.com>
2023-11-26 23:03:42 +0200

    complete home and abot page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>RQRNG</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Accusantium blanditiis quasi voluptatem a quisquam iusto distinctio iure eius odit laboriosam corrupti in dolores itaque possimus, 
+        porro ratione laborum saepe omnis vel sequi maxime fugiat quis molestiae? Quae voluptatibus soluta nulla atque quod magnam numquam neque voluptate, 
+        quia sint libero, dolores earum assumenda dolorem obcaecati consequuntur mollitia, impedit expedita non. Reprehenderit, eos. 
+        Recusandae distinctio veniam obcaecati laudantium quia? In, porro doloribus?
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Accusantium blanditiis quasi voluptatem a quisquam iusto distinctio iure eius odit laboriosam corrupti in dolores itaque possimus, 
+        porro ratione laborum saepe omnis vel sequi maxime fugiat quis molestiae? Quae voluptatibus soluta nulla atque quod magnam numquam neque voluptate, 
+        quia sint libero, dolores earum assumenda dolorem obcaecati consequuntur mollitia, impedit expedita non. Reprehenderit, eos. 
+        Recusandae distinctio veniam obcaecati laudantium quia? In, porro doloribus?
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Accusantium blanditiis quasi voluptatem a quisquam iusto distinctio iure eius odit laboriosam corrupti in dolores itaque possimus, 
+        porro ratione laborum saepe omnis vel sequi maxime fugiat quis molestiae? Quae voluptatibus soluta nulla atque quod magnam numquam neque voluptate, 
+        quia sint libero, dolores earum assumenda dolorem obcaecati consequuntur mollitia, impedit expedita non. Reprehenderit, eos. 
+        Recusandae distinctio veniam obcaecati laudantium quia? In, porro doloribus?
+      </p>
+    </div>
   )
 }
