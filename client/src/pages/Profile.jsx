88fdbfe30f6cf9317e42ed35242e6649939d9949
davisjanis <davislarins@gmail.com>
2023-11-26 22:55:00 +0200

    add sign out user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,8 @@ import {updateUserStart,
   updateUserFailure, 
   deleteUserStart, 
   deleteUserSuccess, 
-  deleteUserFailure} 
+  deleteUserFailure,
+  signOut,} 
   from '../redux/user/userSlice';
 
 export default function Profile() {
@@ -100,6 +101,16 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -167,7 +178,7 @@ export default function Profile() {
           className='text-red-700 cursor-pointer'>
           Delete Account
         </span>
-        <span className='text-red-700 cursor-pointer'>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
           Sign out
         </span>
       </div>
